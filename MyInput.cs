2dbfd04d30f8362557f72714318fb8565aaf24a1
DigitalzombieTLD <christian.gendner@gmx.net>
2022-01-16 21:11:30 +0100

    Add project files.

---
+++ MyInput.cs
@@ -0,0 +1,16 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace TownscaperTemplate
+{
+	public static class MyInput
+	{		
+		public static void GetInput()
+		{		
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+				MelonLogger.Msg("Space pressed!");
+			}
+		}
+	}
+}
---
+++ TownscaperTemplate.cs
@@ -0,0 +1,27 @@
+﻿using MelonLoader;
+
+
+namespace TownscaperTemplate
+{
+    public class LittleEnvironmentMain : MelonMod
+    {		
+		public override void OnApplicationStart()
+		{	
+			MelonLogger.Msg("Mod started");
+		}
+
+
+		public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+		{
+			if(sceneName == "Placemaker")
+			{
+				MelonLogger.Msg("Main scene loaded");
+			}			
+		}
+		
+		public override void OnUpdate()
+		{
+			MyInput.GetInput();			
+		}
+	}
+}
